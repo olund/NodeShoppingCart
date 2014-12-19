b0d2ea060e0b54fd6ed2eef14620698b0a3e05e2
olund <henke.olund@gmail.com>
2014-12-19 14:40:21 +0100

    Cart..

--- controllers/cart.js
+++ controllers/cart.js
@@ -3,6 +3,12 @@
 var db = require('../lib/db');
 
 module.exports = function (router) {
+
+    router.get('/', function (req, res) {
+        res.render('cart/index');
+    });
+
+
     router.post('/:id', function (req, res) {
         req.session.cart = req.session.cart || {};
         var cart = req.session.cart;
@@ -12,8 +18,16 @@ module.exports = function (router) {
             .filter('it.id == id', { id: req.params.id })
             .single(null, null, function (product) {
                 if (cart[req.params.id]) {
-                    cart.req.params.id
+                    cart[req.params.id].qty++;
+                } else {
+                    cart[req.params.id] = {
+                        title: product.title,
+                        price: product.price,
+                        qty: 1
+                    }
                 }
+
+                res.redirect('/cart');
             });
     });
 };
