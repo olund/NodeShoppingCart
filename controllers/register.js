63518f8dc5aee4d98bf6b838f9ab61c3f66c041b
olund <henke.olund@gmail.com>
2015-01-04 02:07:33 +0100

    fixed register user thingy

--- controllers/register.js
+++ controllers/register.js
@@ -8,25 +8,30 @@ module.exports = function (router) {
     });
 
     router.post('/', function (req, res) {
-        db
-            .Users
-            .filter('it.username == username', { username: req.body.username })
-            .first(null, null, function (user) {
-                // Already exist.
+        models.User.find({
+            where: {
+                username: req.body.username
+            },
+            limit: 1
+        }).then(function (user) {
+            if (user) {
+                console.log('KOM HIT', user);
+                // A user already been found, can't create a new account
                 req.flash('error', 'A user with that username already exist');
                 res.redirect('/register');
-            }).fail(function () {
-                var user = new User();
-                user.username = req.body.username;
-                user.password = passwordHash.generate(req.body.password);
-                user.role = 'user';
+                return;
+            }
 
-                db.Users.add(user);
-                db.saveChanges(function () {
-                    // Login the user.
-                    req.session.user = user;
-                    res.redirect('/');
-                });
+            // If the search fails we can create the new user.
+            models.User.create({
+                username: req.body.username,
+                password: passwordHash.generate(req.body.password),
+                role: 'user'
+            }).complete(function (err, user) {
+                // Login the user and redirect.
+                req.session.user = user;
+                res.redirect('/dashboard');
             });
+        });
     });
 };
